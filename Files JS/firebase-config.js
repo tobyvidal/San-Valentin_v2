3b0202f87a154d77afea3a5daaca4e87b72f267e
Tobias Vidal <150728892+tobyvidal@users.noreply.github.com>
2025-02-13 16:33:40 +0100

    Update firebase-config.js

--- Files JS/firebase-config.js
+++ Files JS/firebase-config.js
@@ -7,14 +7,15 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.0/firebase
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-apiKey: "process.env.FIREBASE_API_KEY",
-authDomain: "process.env.AUTHDOMAINV",
-projectId: "process.env.PROJECTID",
-storageBucket: "process.env.STORAGEBUCKETV",
-messagingSenderId: "process.env.MESSAGINGSENDERIDV",
-appId: "process.env.APPID",
-measurementId: "process.env.MEASUREMENTID"
+  apiKey: process.env.FIREBASE_API_KEY, 
+  authDomain: process.env.AUTHDOMAINV,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKETV,
+  messagingSenderId: process.env.MESSAGINGSENDERIDV,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID
 };
+
 
 // Initialize Firebase
 function initializeFirebase(){
